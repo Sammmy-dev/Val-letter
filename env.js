cbb6fdc74d6ddf4c606cfe3a819118322eb3977e
Sammmy-dev <150556970+Sammmy-dev@users.noreply.github.com>
2024-02-14 06:21:41 +0100

    Update env.js

--- env.js
+++ env.js
@@ -1,8 +1,30 @@
 let envelope = document.querySelector(".adaptcont");
+
 let content = document.querySelector(".letter-content");
+
+let cover = document.querySelector(".closed");
+
+
+
 
 
 envelope.addEventListener(`click`, ()=>{
+
     content.style.animationPlayState=`running`;
+
+    envelope.style.animationPlayState=`paused`;
+
+    cover.style.opacity=`0`;
+
+    setTimeout(function() {
+
+        cover.style.backgroundColor=`pink`;
+
+        cover.style.opacity=`1`;
+
+    }, 2300);
+
 })
+
+
 
